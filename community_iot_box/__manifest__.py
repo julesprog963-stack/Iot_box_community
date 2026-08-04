4ac2e969772f3d5c74c91ef576f68273d22660bd
JDA SOLUTIONS <dev@jdasolutions.mx>
2026-08-04 17:37:57 -0600

    chore(18.0): publish current Odoo 18 evidence

--- community_iot_box/__manifest__.py
+++ community_iot_box/__manifest__.py
@@ -25,12 +25,10 @@ Features:
     "depends": ["base", "web"],
     "images": [
         "static/description/main_screenshot.png",
-        "static/description/images/main_screenshot.gif",
-        "static/description/images/dashboard_screenshot.png",
-        "static/description/images/iot_boxes.png",
-        "static/description/images/iot_devices.png",
-        "static/description/images/iot_jobs.png",
-        "static/description/images/iot_box_configuration.png",
+        "static/description/images/iot_box_menu_odoo18.png",
+        "static/description/images/iot_box_configuration_odoo18.png",
+        "static/description/images/iot_box_devices_odoo18.png",
+        "static/description/images/iot_box_jobs_odoo18.png",
     ],
     "data": [
         "security/ir.model.access.csv",
