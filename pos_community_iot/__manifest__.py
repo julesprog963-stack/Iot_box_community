d22e8a56ec8d07b7250778dad7f13ea888503506
JDA SOLUTIONS <dev@jdasolutions.mx>
2026-08-04 13:24:33 -0600

    Mark IoT addons as free

--- pos_community_iot/__manifest__.py
+++ pos_community_iot/__manifest__.py
@@ -9,6 +9,8 @@
     "website": "https://github.com/julesprog963-stack/Iot_box_community",
     "support": "julesprog963@gmail.com",
     "license": "LGPL-3",
+    "price": 0.0,
+    "currency": "USD",
     "images": ["static/description/main_screenshot.gif", "static/description/main_screenshot.png"],
     "depends": ["point_of_sale", "community_iot_box"],
     "data": [
